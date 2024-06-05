7dd41998e6e6502cf9d1e88bdcacc6d0b4fa8acb
chamchi <3chamchi@gmail.com>
2024-06-05 17:58:02 +0900

    줄바꿈 처리

--- section14.py
+++ section14.py
@@ -2,12 +2,16 @@
 
 # 1. ChatGPT API 패키지 불러오기
 from openai import OpenAI
+
 # 2. API 키 변수 생성
 api_key = ""
+
 # 3. ChatGPT 연동
 client = OpenAI(api_key=api_key)
+
 # 4. 생성할 이미지 설명 입력 받기
 prompt = input("생성할 이미지의 설명을 입력해주세요 : ")
+
 # 번역 API 사용
 # 5. ChatGPT API 요청, 응답 변수 생성
 response = client.images.generate(
@@ -15,5 +19,6 @@ response = client.images.generate(
     model="dall-e-2",
     size="256x256",
 )
+
 # 6. 응답 메시지 출력
 print(response.data)
